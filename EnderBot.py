64d2c68f7e0113e36c053dcaf3e185ef04fe94de
enderelijas <50271707+enderelijas@users.noreply.github.com>
2019-08-14 21:30:06 +0300

    H

--- EnderBot.py
+++ EnderBot.py
@@ -20,9 +20,5 @@ async def ping(ctx):
     await ctx.send("Pong :stuck_out_tongue_closed_eyes: ")
 
 
-@bot.command()
-async def info(ctx):
-    embed = discord.Embed(title="EnderBot",description="Here To make this server a better place!")
-
 
 bot.run("NTY1OTUxNDYyNDkzMTI2NjY3.XOmMJA.a4WTS6oh34ppT29-JGtARcfmQM4")
