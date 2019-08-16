8d314590bb34a44b98b167f0f10794e49d7db9bc
ThatGuy5275 <42699143+Wallvon@users.noreply.github.com>
2019-08-16 15:17:51 +0200

    Made kick & ban commands

--- EnderBot.py
+++ EnderBot.py
@@ -25,6 +25,32 @@ async def ping(ctx):
     embed.add_field(name="Ping", value=f'🏓 {round(bot.latency * 1000)}ms')
     await ctx.send(embed=embed)
 
-
+@bot.command()
+@commands.has_role(567737541546082304)
+async def ban(ctx, member: discord.Member, *, reason='No reason provided.'):
+    dm = discord.Embed(title="You have been banned from `Ender Network`!",
+                        description="Details about the ban:", color=embcolor)
+    dm.add_field(name="Moderator:",
+                    value=ctx.message.author.display_name)
+    dm.add_field(name="Reason:", value=f"{reason}")
+    dm.set_thumbnail(url=member.avatar_url)
+    await member.send(embed=dm)  # Send DM
+    await member.ban(reason=reason)  # Ban
+    await ctx.message.delete()  # Delete The Message
+    await ctx.send('The user has been banned.')
+    
+@bot.command()
+@commands.has_role(567737541546082304)
+async def kick(ctx, member: discord.Member, *, reason='No reason provided.'):
+    dm = discord.Embed(
+        color=embcolor, title="You have been kicked from `Ender Network`!")
+    dm.set_thumbnail(url=member.avatar_url)
+    dm.add_field(name="Reason:", value=f"{reason}")
+    dm.add_field(name="Moderator:",
+                    value=ctx.message.author.display_name)
+    await member.send(embed=dm)  # Send DM
+    await member.kick(reason=reason)  # Kick
+    await ctx.message.delete()  # Delete The Message
+    await ctx.send('✅ Moderation action completed')
 
 bot.run(os.getenv('TOKEN1'))
