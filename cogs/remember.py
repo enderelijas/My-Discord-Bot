2d86b778008d7c983d9bd77532404f3e79454a60
ThatGuy5275 <42699143+Wallvon@users.noreply.github.com>
2019-09-03 16:02:06 +0200

    Added some MEE6 functionality

---
+++ cogs/remember.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import tasks, commands
+
+
+class MyCog(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.index = 0
+        self.printer.start()
+
+    def cog_unload(self):
+        self.printer.cancel()
+
+    @tasks.loop(seconds=60.0)
+    async def printer(self):
+        print(self.index)
+        self.index += 1
+        if self.index == 10:
+            channel = self.bot.get_channel(613426733424836653)
+            general = self.bot.get_channel(613426184025407549)
+            await channel.send('Remember to spam here not in {general}')
+            self.index = 0
+
+    @printer.before_loop
+    async def before_printer(self):
+        print('waiting...')
+        await self.bot.wait_until_ready()
+
+
+def setup(bot):
+    bot.add_cog(MyCog(bot))
