32333a004a42294d34998c4d2c872a53d6f5673f
ThatGuy5275 <42699143+Wallvon@users.noreply.github.com>
2019-09-03 16:07:03 +0200

    idk

--- cogs/remember.py
+++ cogs/remember.py
@@ -13,7 +13,7 @@ class MyCog(commands.Cog):
         self.printer.cancel()
 
     @tasks.loop(seconds=60.0)
-    async def printer(self):
+    async def something(self):
         print(self.index)
         self.index += 1
         if self.index == 10:
@@ -22,8 +22,8 @@ class MyCog(commands.Cog):
             await channel.send('Remember to spam here not in {general}')
             self.index = 0
 
-    @printer.before_loop
-    async def before_printer(self):
+    @something.before_loop
+    async def before_something(self):
         print('waiting...')
         await self.bot.wait_until_ready()
 
